62f89e316745b24d1f897641754790104fb3fbf5
William Li <william_jzl@outlook.com>
2025-04-16 00:36:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# --- Data Sources ---
 data "harvester_image" "img" {
   display_name = var.img_display_name
   namespace    = "harvester-public"
@@ -8,6 +9,7 @@ data "harvester_ssh_key" "mysshkey" {
   namespace = var.namespace
 }
 
+# --- Common Resources ---
 resource "random_id" "secret" {
   byte_length = 5
 }
@@ -17,82 +19,13 @@ resource "harvester_cloudinit_secret" "cloud-config" {
   namespace = var.namespace
 
   user_data = templatefile("cloud-init.tmpl.yml", {
-      public_key_openssh = data.harvester_ssh_key.mysshkey.public_key
-    })
-}
-resource "harvester_virtualmachine" "storagevm" {
-  
-  count = 1
-
-  name                 = "${var.username}-storage-${random_id.secret.hex}"
-  namespace            = var.namespace
-  restart_after_update = true
-
-  description = "Cluster storage Node"
-
-  cpu    = 4 
-  memory = "8Gi"
-
-  efi         = true
-  secure_boot = false
-
-  run_strategy    = "RerunOnFailure"
-  hostname        = "${var.username}-storage-${random_id.secret.hex}"
-  reserved_memory = "100Mi"
-  machine_type    = "q35"
-
-  tags = {
-    condenser_ingress_isAllowed     = true
-    condenser_ingress_isEnabled     = true
-    condenser_ingress_node_hostname = "node-${var.username}-x"
-    condenser_ingress_node_port     = 9100
-    condenser_ingress_isEnabled = true
-    condenser_ingress_os_hostname = "${var.username}-s3-x"
-    condenser_ingress_os_port = 9000
-    condenser_ingress_os_protocol = "https"
-    condenser_ingress_os_nginx_proxy-body-size = "100000m"
-    condenser_ingress_cons_hostname = "${var.username}-cons-x"
-    condenser_ingress_cons_port = 9001
-    condenser_ingress_cons_protocol = "https"
-    condenser_ingress_cons_nginx_proxy-body-size = "100000m"
-  }
-
-  network_interface {
-    name           = "nic-1"
-    wait_for_lease = true
-    type           = "bridge"
-    network_name   = var.network_name
-  }
-
-  disk {
-    name       = "rootdisk"
-    type       = "disk"
-    size       = "10Gi"
-    bus        = "virtio"
-    boot_order = 1
-
-    image       = data.harvester_image.img.id
-    auto_delete = true
-  }
-
-  disk {
-    name       = "datadisk"
-    type       = "disk"
-    size       = "200Gi"
-    bus        = "virtio"
-    boot_order = 2
-
-    auto_delete = true
-  }
-
-  cloudinit {
-    user_data_secret_name = harvester_cloudinit_secret.cloud-config.name
-  }
+    public_key_openssh = data.harvester_ssh_key.mysshkey.public_key
+  })
 }
 
+# --- Host Virtual Machine (Matches Requirement Table: Host) ---
 resource "harvester_virtualmachine" "hostvm" {
-  
-  count = 1
+  count = 1 # Requirement: 1 Host VM
 
   name                 = "${var.username}-host-${random_id.secret.hex}"
   namespace            = var.namespace
@@ -100,8 +33,8 @@ resource "harvester_virtualmachine" "hostvm" {
 
   description = "Cluster Head Node"
 
-  cpu    = 2
-  memory = "4Gi"
+  cpu    = 2    # Requirement: 2 Cores
+  memory = "4Gi" # Requirement: 4GB RAM
 
   efi         = true
   secure_boot = false
@@ -112,15 +45,15 @@ resource "harvester_virtualmachine" "hostvm" {
   machine_type    = "q35"
 
   tags = {
-    condenser_ingress_isAllowed              = true
-    condenser_ingress_isEnabled              = true
-   condenser_ingress_prometheus_hostname    = "prometheus-${var.username}-x"
-   condenser_ingress_prometheus_port        = 9090
-   condenser_ingress_nodeexporter_hostname  = "nodeexporter-${var.username}-x"
-   condenser_ingress_nodeexporter_port      = 9100
-   condenser_ingress_grafana_hostname       = "grafana-${var.username}-x"
-   condenser_ingress_grafana_port           = 3000
- }
+    condenser_ingress_isAllowed           = true
+    condenser_ingress_isEnabled           = true
+    condenser_ingress_prometheus_hostname = "prometheus-${var.username}-x"
+    condenser_ingress_prometheus_port     = 9090
+    condenser_ingress_nodeexporter_hostname = "nodeexporter-${var.username}-x"
+    condenser_ingress_nodeexporter_port   = 9100
+    condenser_ingress_grafana_hostname    = "grafana-${var.username}-x"
+    condenser_ingress_grafana_port        = 3000
+  }
 
   network_interface {
     name           = "nic-1"
@@ -132,31 +65,32 @@ resource "harvester_virtualmachine" "hostvm" {
   disk {
     name       = "rootdisk"
     type       = "disk"
-    size       = "10Gi"
+    size       = "10Gi" # Requirement: 10GB HDD1
     bus        = "virtio"
     boot_order = 1
 
     image       = data.harvester_image.img.id
     auto_delete = true
   }
+  # No HDD2 as per requirement
 
   cloudinit {
     user_data_secret_name = harvester_cloudinit_secret.cloud-config.name
   }
 }
 
+# --- Worker Virtual Machines (Matches Requirement Table: Worker) ---
 resource "harvester_virtualmachine" "workervm" {
-  
-  count = var.vm_count
+  count = 4 # Requirement: 4 Worker VMs
 
   name                 = "${var.username}-worker-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
   namespace            = var.namespace
   restart_after_update = true
 
-  description = "Cluster Compute Node"
+  description = "Cluster Compute Node ${count.index + 1}"
 
-  cpu    = 4
-  memory = "32Gi"
+  cpu    = 4     # Requirement: 4 Cores
+  memory = "32Gi" # Requirement: 32GB RAM
 
   efi         = true
   secure_boot = false
@@ -166,15 +100,15 @@ resource "harvester_virtualmachine" "workervm" {
   reserved_memory = "100Mi"
   machine_type    = "q35"
 
- tags = {
-   condenser_ingress_isAllowed     = true
-   condenser_ingress_isEnabled     = true
-   condenser_ingress_node_hostname = "node-${var.username}-x"
-   condenser_ingress_node_port     = 9100
- }
+  tags = {
+    condenser_ingress_isAllowed     = true
+    condenser_ingress_isEnabled     = true
+    condenser_ingress_node_hostname = "node-${var.username}-x" # Consider making this unique per node if needed
+    condenser_ingress_node_port     = 9100
+  }
 
   network_interface {
-    name           = "nic-1"
+    name           = "nic-1-${count.index}" # Ensure unique name per VM if needed, though Harvester might handle this
     wait_for_lease = true
     type           = "bridge"
     network_name   = var.network_name
@@ -183,12 +117,22 @@ resource "harvester_virtualmachine" "workervm" {
   disk {
     name       = "rootdisk"
     type       = "disk"
-    size       = "25Gi"
+    size       = "50Gi" # Requirement: 50GB HDD1
     bus        = "virtio"
     boot_order = 1
 
     image       = data.harvester_image.img.id
     auto_delete = true
+  }
+
+  disk {
+    name       = "datadisk" # Requirement: HDD2
+    type       = "disk"
+    size       = "200Gi" # Requirement: (up to 200GB) - Set to max specified
+    bus        = "virtio"
+    boot_order = 2      # Make sure root disk is boot_order = 1
+
+    auto_delete = true # Usually desired for non-persistent data disks
   }
 
   cloudinit {
