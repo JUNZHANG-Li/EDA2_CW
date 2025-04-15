769e32a2b91de1d23832e7ddaf46f4a69e6ad47d
William Li <william_jzl@outlook.com>
2025-04-16 00:45:06 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,13 +5,7 @@ output worker_vm_ips {
 output worker_vm_ids {
   value = harvester_virtualmachine.workervm.*.id
 }
-output storage_vm_ips {
-  value = harvester_virtualmachine.storagevm[*].network_interface[0].ip_address
-}
 
-output storage_vm_ids {
-  value = harvester_virtualmachine.storagevm.*.id
-}
 output host_vm_ips {
   value = harvester_virtualmachine.hostvm[*].network_interface[0].ip_address
 }
